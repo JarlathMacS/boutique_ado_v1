abc166aed641bf23e7878ea87289137b4634d65d
Jarlath <jarlath@jarlath.cc>
2026-03-11 00:20:15 -0500

    add home app and index template

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """View function to return the home page of the site."""
+    return render(request, 'home/index.html')
