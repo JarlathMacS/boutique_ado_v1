7181d6bbe285b39d83334f22aba63cc06ec3fef3
Jarlath <jarlath@jarlath.cc>
2026-03-23 22:55:13 -0500

    add sorting

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+
+from home import models
 from .models import Category, Product
 # Create your views here.
 
@@ -11,8 +13,24 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(
+                    lower_name=models.functions.Lower('name')
+                )
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -32,10 +50,13 @@ def all_products(request):
             )
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
