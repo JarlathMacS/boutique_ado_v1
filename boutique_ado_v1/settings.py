3a31dac67a066744998bd703794c56a201fedc26
Jarlath <jarlath@jarlath.cc>
2026-05-15 21:42:44 -0500

    map webhooks to custom handlers, test webhook events

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -30,8 +30,10 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '127.0.0.1',  # local host
+    'localhost',  # listen for stripe webhooks
+]
 
 # Application definition
 
@@ -199,3 +201,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
