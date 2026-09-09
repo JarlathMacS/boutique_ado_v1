68c6528b8bfd5357f21c144adb63ca12b3555078
Jarlath <jarlath@jarlath.cc>
2026-09-08 23:20:32 -0500

    add cache control

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -204,9 +204,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     # Cache control
-    # AWS_S3_OBJECT_PARAMETERS = {
-    #     'CacheControl': 'max-age=86400',
-    # }
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado--v1-1d222e016c9a'
