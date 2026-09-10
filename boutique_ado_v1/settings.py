53e7a71e2a1f92bbaf0f8213ce3510944c6d95f7
Jarlath <jarlath@jarlath.cc>
2026-09-10 01:07:44 -0500

    cleanup comments in settings

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -215,13 +215,14 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-    # Static and Media Files
     AWS_S3_CUSTOM_DOMAIN = (
         # f'{AWS_STORAGE_BUCKET_NAME}.s3.'
         f'{AWS_STORAGE_BUCKET_NAME}.s3-website.'
         f'{AWS_S3_REGION_NAME}.amazonaws.com'
         # 'amazonaws.com'
     )
+
+    # Static and Media Files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
