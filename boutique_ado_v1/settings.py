ac9b4371bd429420c671115b49927d6c97dc8f93
Jarlath <jarlath@jarlath.cc>
2026-09-20 21:49:23 -0500

    undo test email settings

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -245,9 +245,9 @@ if 'DEVELOPMENT' in os.environ:
 else:
     # Email
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'jarlath.code@gmail.com'
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = 'jarlath.code@gmail.com'
-    EMAIL_HOST_PASSWORD = 'hvmjtxzhfezjtekf'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
